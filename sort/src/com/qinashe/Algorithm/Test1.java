9304d4e3608dca7eda0aef4b06e2c5960cd42ea6
qianshe <cutoffother@aliyun.com>
2020-08-04 17:37:57 +0800

    Algorithm

--- sort/src/com/qinashe/Algorithm/Test1.java
+++ sort/src/com/qinashe/Algorithm/Test1.java
@@ -14,6 +14,10 @@ public class Test1 {
     public void test01() {
         int[] arr1 = {2, 3, 4, 5, 6, 7};
         int[] arr2 = {4, 5, 6, 7, 8, 9};
+        test01(arr1, 0, arr1.length - 1, arr2, 0, arr2.length - 1);
+    }
 
+    public int test01(int[] arr1, int l1, int r1, int[] arr2, int l2, int r2) {
+        return 0;
     }
 }
